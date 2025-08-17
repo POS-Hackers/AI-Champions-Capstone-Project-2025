5f210cf816900254c08ff43495e479d16a4f3974
Hizam <hizam.kamis@gmail.com>
2025-08-17 16:48:20 +0800

    added APP_PASSWORD

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ def check_password():
     """Returns `True` if the user had the correct password."""  
     def password_entered():  
         """Checks whether a password entered by the user is correct."""  
-        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):  
+        if hmac.compare_digest(st.session_state["password"], st.secrets["APP_PASSWORD"]):  
             st.session_state["password_correct"] = True  
             del st.session_state["password"]  # Don't store the password.  
         else:  
